8fb24efbad7dbac1f2bee2b16f057a1ae91bbc56
rajkaranswain <rajkaranswain16@gmail.com>
2021-02-25 19:30:51 +0530

    tuple and dictionary

---
+++ variable/dictionarydemo.py
@@ -0,0 +1,5 @@
+student_grade= {"raju":9.8,"samal":8.7,"sanket":7.0}
+#average of student
+tsum = sum(student_grade.values())
+mean= tsum/(len(student_grade))
+print(mean)
---
+++ variable/tupledemo.py
@@ -0,0 +1,4 @@
+odisha_temp=(30,40,45)
+print(odisha_temp)
+print("tuple are not mutable but the list are mutable")
+print(type(odisha_temp))
